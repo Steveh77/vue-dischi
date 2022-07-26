01d66edef6f5a814e9bfb55b7e82c11a49a9a0aa
Steveh77 <stederosa76@gmail.com>
2022-07-26 18:13:20 +0200

    added main and albums(esercizio base finito)

---
+++ src/components/MyMain.vue
@@ -0,0 +1,47 @@
+@<template>
+    <div class="bg-grey">
+        <div class="container">
+            <div class="row d-flex justify-content-center p-5">
+                <div v-for="(album, i) in albums" :key="i" class="card col-lg-2 col-md-5 col-sm-10 m-3 text-center">
+                    <SingleAlbum :poster="album.poster" :title="album.title" :author="album.author"
+                        :year="album.year" />
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import SingleAlbum from "./SingleAlbum.vue";
+export default {
+    name: "SpotifyAlbums",
+    data() {
+        return {
+            albums: []
+        };
+    },
+    mounted() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+            console.log(this.albums);
+            this.albums = res.data.response;
+        });
+    },
+    components: { SingleAlbum }
+}
+</script>
+
+<style lang="scss" scoped>
+.bg-grey {
+    background-color: rgb(30, 45, 59);
+}
+
+.card {
+    min-height: 300px;
+    padding: 25px;
+    border-radius: 0;
+    background-color: rgb(46, 58, 70);
+
+}
+</style>
---
+++ src/components/SingleAlbum.vue
@@ -0,0 +1,34 @@
+@<template>
+    <div>
+        <img class="img-fluid" :src="poster" alt="">
+        <h5>{{ title }}</h5>
+        <h6>{{ author }}</h6>
+        <h6>{{ year }}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleAlbum",
+    props: {
+        poster: String,
+        title: String,
+        author: String,
+        year: String
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+h5 {
+    display: flex;
+    justify-content: center;
+    color: white;
+    margin-top: 5px;
+}
+
+h6 {
+    color: rgb(115, 119, 122);
+}
+</style>
