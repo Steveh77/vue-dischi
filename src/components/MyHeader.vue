3324821ae45a13aba36c201453984929f512a3e9
Steveh77 <stederosa76@gmail.com>
2022-07-26 16:25:05 +0200

    added Header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <i class="fa-brands fa-spotify"></i>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+nav{
+    display: flex;
+    align-items: center;
+    height: 100px;
+    background-color: rgb(46,58,70);
+    i{
+        font-size: 75px;
+        color: rgb(30,215,96);
+    }
+}
+
+</style>
